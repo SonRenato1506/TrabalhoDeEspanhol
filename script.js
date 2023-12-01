7b8c89182e14d60d0f5a979d393dd0895ac7e5b3
Renato Matos <108771359+SonRenato1506@users.noreply.github.com>
2023-12-01 10:11:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function blue () {
+    document.body.style.backgroundImage = "url(azul.jpeg)"
+}
+
+function red () {
+    document.body.style.backgroundImage = "url(red.jpeg)"
+}
+
+function purple () {
+    document.body.style.backgroundImage = "url(roxo.jpeg)"
+}
